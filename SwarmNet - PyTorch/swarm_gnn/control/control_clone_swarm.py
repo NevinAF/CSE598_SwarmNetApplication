ba7d9616266f1ff25dfcf32a2950a38fcb7acbac
anhardy1 <ahardy313@gmail.com>
2022-04-25 12:26:18 -0700

    predict state length parameter for control script

--- SwarmNet - PyTorch/swarm_gnn/control/control_clone_swarm.py
+++ SwarmNet - PyTorch/swarm_gnn/control/control_clone_swarm.py
@@ -9,20 +9,20 @@ def start(model_path):
 
 
 @torch.no_grad()
-def control(last_steps, model, predict_steps):
+def control(last_steps, model, predict_steps, predict_state_length):
     device = 'cpu'
     last_steps = numpy.array(last_steps)
     last_steps = numpy.swapaxes(last_steps, 0, 1)
     if last_steps.shape[1] >= 7:
         # TODO extend to include radius or environmental context
-        last_steps = last_steps[:, :, 0:6]
+        last_steps = last_steps[:, :, 0:predict_state_length]
         last_steps = torch.tensor(last_steps)
         last_steps = last_steps.to(device)
         control_vel = model.forward(last_steps.float(), predict_steps)
         control_vel = control_vel.cpu().detach().numpy()
         control_vel = numpy.swapaxes(control_vel, 0, 1)
     else:
-        control_vel = numpy.zeros([last_steps.shape[0],1,last_steps.shape[2]])
+        control_vel = numpy.zeros([last_steps.shape[0], 1, last_steps.shape[2]])
         control_vel[:, :, :] = last_steps[:, 0:1, :]
 
     return control_vel
